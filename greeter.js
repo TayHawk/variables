cf761453f825488613efa322d1f9c82be3ac5ebf
Taylor Hockley <77340878+TayHawk@users.noreply.github.com>
2021-03-04 13:34:35 -0500

    updated greeter.js
    Added in comments

--- greeter.js
+++ greeter.js
@@ -1,5 +1,12 @@
+/*
+Write a program named greeter.js that greets 'Victor' three times. 
+Your program should use a variable and not hard code the string value 'Victor' in each greeting.
+*/
+
+
 let name =' Victor';
 name= 'Taylor'
+//changed to my name to experiment
 console.log(`Good Morning,${name}.`);
 console.log(`Good Afternoon,${name}.`);
 console.log(`Good Evening,${name}.`);

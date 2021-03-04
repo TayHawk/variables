1e08f9276dd632e56c96198a8d943494abca08e3
Taylor Hockley <77340878+TayHawk@users.noreply.github.com>
2021-03-04 13:35:43 -0500

    updated age.js
    added comments

--- age.js
+++ age.js
@@ -1,3 +1,8 @@
+/*
+Write a program named age.js that includes someone's age and 
+then calculates and reports the future age in 10, 20, 30 and 40 years. 
+*/
+
 let age = 20;
 console.log(`You are ${age} years old.`)
 console.log(`In 10 years, you will be ${age +10} years old.`);

3d7883db017b1788a4943903ad20139457006c0f
Taylor Hockley <taylor.hockleys@gmail.com>
2021-03-04 21:16:55 +0000

    modified age.js

--- age.js
+++ age.js
@@ -3,10 +3,23 @@ Write a program named age.js that includes someone's age and
 then calculates and reports the future age in 10, 20, 30 and 40 years. 
 */
 
-let age = 20;
-console.log(`You are ${age} years old.`)
+let readSyn = require('readline-sync');
+let age = parseInt(readSyn.question('How old are you? '));
+console.log(`You are ${age} years old.`);
 console.log(`In 10 years, you will be ${age +10} years old.`);
 console.log(`In 20 years, you will be ${age +20} years old.`);
 console.log(`In 30 years, you will be ${age +30} years old.`);
 console.log(`In 40 years, you will be ${age +40} years old.`);
 
+/*
+
+let readlineSync = require('readline-sync');
+let age = readlineSync.question('How old are you? ');
+age = parseInt(age);
+console.log(`You are ${age} years old.`);
+console.log(`In 10 years, you will be ${age + 10} years old.`);
+console.log(`In 20 years, you will be ${age + 20} years old.`);
+console.log(`In 30 years, you will be ${age + 30} years old.`);
+console.log(`In 40 years, you will be ${age + 40} years old.`);
+
+*/
